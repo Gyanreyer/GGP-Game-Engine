799bac3ededb1a3636f66c90e8dfb3fd3b12f385
Ryan Geyer <gyanreyer@gmail.com>
2017-10-10 14:32:12 -0400

    Added MovingEnemy script to make enemies move back and forth

---
+++ Unity Protoype/GGP Prototype/Assets/Scripts/MovingEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEnemy : MonoBehaviour {
+
+    Vector3 startPt;
+
+	// Use this for initialization
+	void Start () {
+        startPt = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = startPt + transform.right * Mathf.Sin(Time.time * 1.25f) * 3;	
+	}
+}
