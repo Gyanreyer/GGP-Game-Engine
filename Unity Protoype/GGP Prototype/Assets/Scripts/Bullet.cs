5f837d3b4d82aa9ca547690c490188872a58cd30
Kevin Idzik <kdi4293@rit.edu>
2017-10-10 13:02:18 -0400

    Initial Bullets

---
+++ Unity Protoype/GGP Prototype/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private GameObject shooter;
+    private float speed = .25f; //The speed of the bullet
+
+    void FixedUpdate() //Physics
+    {
+        transform.Translate(shooter.transform.forward * speed);
+    }
+
+    private void OnCollisionEnter(Collision coll) //Colliding with objects
+    {
+        if (coll.gameObject.tag == "target")
+        {
+            Destroy(coll.gameObject); //Destroy target
+            Destroy(gameObject); //Destroy bullet
+        }
+        else if (coll.gameObject.tag == "ground")
+            Destroy(gameObject); //Destroy bullet
+    }
+}
---
+++ Unity Protoype/GGP Prototype/Assets/Scripts/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletPoint;
+
+    void Update() //Update is called once per frame
+    {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Pew");
+            Instantiate(bullet, bulletPoint.position, bulletPoint.rotation);
+        }
+	}
+}
