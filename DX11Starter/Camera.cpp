e562e31bfc6147bb011e5325506b7a789f16a658
Kevin Idzik <kdi4293@ad.rit.edu>
2017-10-16 15:25:25 -0400

    Quaternion Camera
    -Flipping Y-axis issue still present

--- DX11Starter/Camera.cpp
+++ DX11Starter/Camera.cpp
@@ -1,36 +1,103 @@
+///Kevin Idzik
+///First person camera
 #include "Camera.h"
 
+//Constructor
 Camera::Camera()
 {
 }
 
+//Parameterized constructor
 Camera::Camera(unsigned int width, unsigned int height)
 {
-	transform = Transform(XMFLOAT3(0,0,-5),XMFLOAT3(0,0,0),XMFLOAT3());
+	XMStoreFloat4x4(&viewMatrix, XMMatrixIdentity()); //Store the identity matrix in the view matrix
+	XMStoreFloat4x4(&projectionMatrix, XMMatrixIdentity()); //Store the identity matrix in the projection matrix
+	position = XMFLOAT3(0, 0, -5); //Default position is at world origin
 
-	UpdateProjectionMatrix(width, height);
+	forward = XMFLOAT3(0, 0, 1); //The camera's forward vector
+	right = XMFLOAT3(1, 0, 0); //The camera's right vector
+	up = XMFLOAT3(0, 1, 0); //The camera's up vector
 
+	rotation = XMFLOAT3(0, 0, 0); //Default rotation is none
+	movementSpeed = 2; //The camera's movement speed
 	rotationSpeed = .5f; //The camera's rotation speed
+
+	//Initialize the view matrix
+	UpdateViewMatrix();
+
+	//Initialize the projection matrix
+	UpdateProjectionMatrix(width, height);
 }
 
+//Destructor
 Camera::~Camera()
 {
 }
 
-Transform * Camera::GetTransform()
-{
-	return &transform;
-}
-
+//Get the camera's view matrix
 XMFLOAT4X4 Camera::GetViewMatrix()
 {
-	UpdateViewMatrix();//Make sure viewMatrix is up to date before returning
 	return viewMatrix;
 }
 
+//Get the camera's projection matrix
 XMFLOAT4X4 Camera::GetProjectionMatrix()
 {
 	return projectionMatrix;
+}
+
+//Set the camera's rotation
+void Camera::SetRotation(XMFLOAT3 rot)
+{
+
+}
+
+//Handles keyboard input
+void Camera::KeyboardInput(float deltaTime)
+{
+	XMVECTOR normalizedForward = XMVector3Normalize(XMLoadFloat3(&forward)); //Create a normalized forward vector for movement
+	XMVECTOR up = XMVectorSet(0, 1, 0, 0); //The camera's up vector
+
+	//Load the position FLOAT3 as an XMVECTOR
+	XMVECTOR pos = XMLoadFloat3(&position);
+
+	//Keyboard inputs use a bit mask because we only need the high bit
+	//Move the camera for WASD controls
+	//Space and X control vertical movement
+	//Move forward
+	if (GetAsyncKeyState('W') & 0x8000)
+		pos += normalizedForward * movementSpeed * deltaTime;
+	//Strafe left
+	if (GetAsyncKeyState('A') & 0x8000)
+		pos += XMVector3Cross(normalizedForward, up) * movementSpeed * deltaTime; //Cross the forward vector and the up axis
+	//Move backward
+	if (GetAsyncKeyState('S') & 0x8000)
+		pos -= normalizedForward * movementSpeed * deltaTime;
+	//Strafe right
+	if (GetAsyncKeyState('D') & 0x8000)
+		pos -= XMVector3Cross(normalizedForward, up) * movementSpeed * deltaTime; //Cross the forward vector and the up axis
+	//Move down
+	if (GetAsyncKeyState('X') & 0x8000)
+		pos -= up * movementSpeed * deltaTime;
+	//Move up
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+		pos += up * movementSpeed * deltaTime;
+
+	//Store the position back into an XMVECTOR
+	XMStoreFloat3(&position, pos);
+}
+
+//Updates the view matrix
+void Camera::UpdateViewMatrix()
+{
+	XMFLOAT3 unitForward = XMFLOAT3(0, 0, 1); //Create a unit forward vector for transformations
+
+	XMVECTOR updatedRotation = XMQuaternionRotationRollPitchYaw(rotation.x, rotation.y, rotation.z); //Create a new rotation quaternion
+	XMLoadFloat3(&forward) = XMVector3Rotate(XMLoadFloat3(&unitForward), updatedRotation); //Apply the new rotation matrix to the camera's forward vector
+
+	XMMATRIX updatedViewMatrix = XMMatrixLookToLH(XMLoadFloat3(&position), XMLoadFloat3(&forward), XMVectorSet(0, 1, 0, 0)); //Update the view matrix
+
+	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(updatedViewMatrix)); //Transpose and update the camera's view matrix
 }
 
 //Updates the camera's view matrix
@@ -38,18 +105,19 @@ void Camera::Update(float deltaTime)
 {
 	UpdateViewMatrix();
 
-	KeyboardInput(deltaTime); //Handle keyboard input
+	KeyboardInput(deltaTime); //Handle keybaord input
 }
 
+//Updates the projection matrix
 void Camera::UpdateProjectionMatrix(unsigned int width, unsigned int height)
 {
-	//Create and store projection matrix based on given width/height
-	XMStoreFloat4x4(&projectionMatrix, XMMatrixTranspose(
-		XMMatrixPerspectiveFovLH(0.25f * XM_PI,		// Field of View Angle
-			(float)width / height,		// Aspect ratio
-			0.1f,	// Near clip plane distance
-			100.0f))	// Far clip plane distance
-	);
+	//Matches the window's aspect ratio, updates on window resize
+	//Field of view angle
+	//Aspect ratio
+	//Near clip plane distance
+	//Far clip plane distance
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f * 3.1415926535f, (float)width / height, 0.1f, 100.0f);
+	XMStoreFloat4x4(&projectionMatrix, XMMatrixTranspose(P)); //Transpose for HLSL
 }
 
 //Handles mouse input
@@ -60,126 +128,25 @@ void Camera::MouseInput(float xAxis, float yAxis)
 	float xRadianized = xAxis / 200;
 	float yRadianized = yAxis / 200;
 
-	XMVECTOR quaternion = XMQuaternionRotationRollPitchYaw(yRadianized * rotationSpeed, xRadianized * rotationSpeed, 0);
+	//Calculate rotations
+	rotation.x += yRadianized * rotationSpeed; //Rotate around the Y axis
+	rotation.y += xRadianized * rotationSpeed; //Rotate around the X axis
 
-	XMVECTOR forward = XMVectorSet(0, 0, 1, 0);
-	XMVECTOR right = XMVectorSet(1, 0, 0, 0);
-	XMVECTOR up = XMVectorSet(0, 1, 0, 0);
+	//Calculate quaternion
+	XMVECTOR quaternion = XMQuaternionRotationRollPitchYaw(rotation.x, rotation.y, 0);
 
-	XMVECTOR updatedForward = XMVector3Rotate(forward, quaternion);
-	XMVECTOR updatedRight = XMVector3Rotate(right, quaternion);
-	XMVECTOR updatedUp = XMVector3Rotate(up, quaternion);
+	//Unit vectors
+	XMVECTOR unitForward = XMVectorSet(0, 0, 1, 0);
+	XMVECTOR unitRight = XMVectorSet(1, 0, 0, 0);
+	XMVECTOR unitUp = XMVectorSet(0, 1, 0, 0);
 
-	XMFLOAT3 storedQuaternion;
-	XMStoreFloat3(&storedQuaternion, quaternion);
-	//transform.Rotate(storedQuaternion.x, storedQuaternion.y, storedQuaternion.z);
+	//Update vectors
+	XMVECTOR updatedForward = XMVector3Rotate(unitForward, quaternion);
+	XMVECTOR updatedRight = XMVector3Rotate(unitRight, quaternion);
+	XMVECTOR updatedUp = XMVector3Rotate(unitUp, quaternion);
 
-	XMStoreFloat3(&transform.forward, forward);
-	XMStoreFloat3(&transform.right, right);
-	XMStoreFloat3(&transform.up, up);
-
-	//Rotate around the X and Y axis
-	//transform.Rotate(yRadianized * rotationSpeed, xRadianized * rotationSpeed, 0);
-
-	////"Clamp" the rotation within 360 and -360
-	////Except these are radians, so clamp between 3.14159 and -3.14159
-	////Rotation only occurs on the X and Y axes, so the Z axis can always be 0
-	//if (transform.rotation.x > TWO_PI || transform.rotation.x < -TWO_PI)
-	//	transform.SetRotation(0, transform.rotation.y, 0);
-	//if (transform.rotation.y > TWO_PI || transform.rotation.y < -TWO_PI)
-	//	transform.SetRotation(transform.rotation.x, 0, 0);
-
-	////If the camera is twisted around itself
-	//if (abs(transform.rotation.x) > PI && abs(transform.rotation.y) > PI)
-	//{
-	//	if (transform.rotation.x > transform.rotation.y)
-	//		transform.SetRotation(abs(transform.rotation.x) - PI, 0, 0);
-	//	else if (transform.rotation.x < transform.rotation.y)
-	//		transform.SetRotation(0, abs(transform.rotation.y) - PI, 0);
-	//	else
-	//		transform.SetRotation(0, 0, 0);
-	//}
-
-	printf("Rotation ");
-	printVector(transform.rotation);
-	//printf("Up ");
-	//printVector(transform.up);
-	//printf("Forward ");
-	//printVector(transform.forward);
-	//printf("Right ");
-	//printVector(transform.right);
-	//printf("\n");
-}
-
-//Handles keyboard input
-void Camera::KeyboardInput(float deltaTime)
-{
-	XMVECTOR fwd = XMLoadFloat3(&transform.GetForward());
-
-	XMVECTOR normalizedForward = XMVector3Normalize(fwd); //Create a normalized forward vector for movement
-	XMVECTOR up = XMVectorSet(0, 1, 0, 0); //The camera's up vector
-
-	float speed = 2.0f * deltaTime; //Movement speed
-
-	//How much to move by in each direction this frame
-	float forwardSpeed = 0.0f;
-	float sideSpeed = 0.0f;
-	float upSpeed = 0.0f;
-
-	//Keyboard inputs use a bit mask because we only need the high bit
-	if (GetAsyncKeyState('W') & 0x8000) {
-		forwardSpeed += speed; //If player pressed W to go forward, add to forward speed
-	}
-	if (GetAsyncKeyState('S') & 0x8000) {
-		forwardSpeed -= speed; //If player pressed S to go backwards, subtract from forward speed
-	}
-	if (GetAsyncKeyState('A') & 0x8000) {
-		sideSpeed += speed; //If player pressed A to go left, add to side speed
-	}
-	if (GetAsyncKeyState('D') & 0x8000) {
-		sideSpeed -= speed; //If player pressed D to go right, subtract from side speed
-	}
-	if (GetAsyncKeyState('Z') & 0x8000) {
-		upSpeed += speed; //If player pressed left shift to go up, add to up speed
-	}
-	if (GetAsyncKeyState('X') & 0x8000) {
-		upSpeed -= speed; //If player pressed left control to go down, subtract from up speed
-	}
-
-	transform.MoveRelative(forwardSpeed, sideSpeed, upSpeed); //Move the camera with directional speeds from input
-	/*
-	//Move the camera for WASD controls
-	//Space and X control vertical movement
-	//Move forward
-	if (GetAsyncKeyState('W') & 0x8000)
-		transform.SetPosition(normalizedForward * movementSpeed * deltaTime);
-	//Strafe left
-	if (GetAsyncKeyState('A') & 0x8000)
-		transform.SetPosition(XMVector3Cross(normalizedForward, up) * movementSpeed * deltaTime); //Cross the forward vector and the up axis
-	//Move backward
-	if (GetAsyncKeyState('S') & 0x8000)
-		transform.SetPosition(-normalizedForward * movementSpeed * deltaTime);
-	//Strafe right
-	if (GetAsyncKeyState('D') & 0x8000)
-		transform.SetPosition(-XMVector3Cross(normalizedForward, up) * movementSpeed * deltaTime); //Cross the forward vector and the up axis
-	//Move down
-	if (GetAsyncKeyState('X') & 0x8000)
-		transform.SetPosition(-up * movementSpeed * deltaTime);
-	//Move up
-	if (GetAsyncKeyState(VK_SPACE) & 0x8000)
-		transform.SetPosition(up * movementSpeed * deltaTime);*/
-}
-
-//Update view matrix if transform has changed
-void Camera::UpdateViewMatrix()
-{
-	if (transform.MatrixNeedsUpdate()) {
-		//Calculate view matrix with XMMatrixLookToLH
-		XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(
-			XMMatrixLookToLH(XMLoadFloat3(&transform.GetPosition()),
-				XMLoadFloat3(&transform.GetForward()),
-				XMVectorSet(0,1,0,0))));
-
-		transform.DoneUpdating();//Notify transform that matrix successfully updated
-	}
+	//Store the updated vectors
+	XMStoreFloat3(&forward, updatedForward);
+	XMStoreFloat3(&right, updatedRight);
+	XMStoreFloat3(&up, updatedUp);
 }
--- DX11Starter/Camera.h
+++ DX11Starter/Camera.h
@@ -1,36 +1,45 @@
+///Kevin Idzik
+///First person camera
 #pragma once
-#include <DirectXMath.h>
-#include <Windows.h> //Input
-#include "Transform.h"
-#include "Macro.h" //Macros
 
-using namespace DirectX;
+#include <DirectXMath.h> //DXMath library
+#include <Windows.h> //Input
+
+using namespace DirectX; //Less unnecessary repetition
 
 class Camera
 {
-public:
-	Camera();
-	Camera(unsigned int width, unsigned int height);
-	~Camera();
-
-	Transform * GetTransform();//Get reference to transform
-
-	//Getters for matrices
-	XMFLOAT4X4 GetViewMatrix();
-	XMFLOAT4X4 GetProjectionMatrix();
-
-	void Update(float deltaTime); //Updates the camera's view matrix
-	void UpdateProjectionMatrix(unsigned int width, unsigned int height); //Update projection matrix using given width and height
-	void MouseInput(float xAxis, float yAxis); //Handles mouse input
-
 private:
-	Transform transform;
+	XMFLOAT4X4 viewMatrix; //Describes where the viewer is in the scene
+	XMFLOAT4X4 projectionMatrix; //Maps the 3D scene onto the 2D screen
 
-	XMFLOAT4X4 projectionMatrix; //Matrix to project cam's view in 2d for rendering
-	XMFLOAT4X4 viewMatrix;
+	//XMVECTOR and XMMATRIX are used for calculation
+	//XMFLOAT3 is used for storage/passing the data around
+	//Could make these XMFLOAT3, but this would require more method calls in the class
+	XMFLOAT3 position; //The camera's position
 
+	XMFLOAT3 forward; //The camera's forward vector
+	XMFLOAT3 right; //The camera's right vector
+	XMFLOAT3 up; //The camera's up vector
+
+	XMFLOAT3 rotation; //The camera's rotation
+	float movementSpeed; //The camera's movement speed
 	float rotationSpeed; //The camera's rotation speed
 
 	void KeyboardInput(float deltaTime); //Handles keyboard input
-	void UpdateViewMatrix();
+	void UpdateViewMatrix(); //Updates the view matrix
+
+public:
+	Camera(); //Constructor
+	Camera(unsigned int width, unsigned int height); //Parameterized constructor
+	~Camera(); //Destructor
+
+	//Accessors and mutators
+	XMFLOAT4X4 GetViewMatrix();
+	XMFLOAT4X4 GetProjectionMatrix();
+	void SetRotation(XMFLOAT3 rot);
+
+	void Update(float deltaTime); //Updates the camera's view matrix
+	void UpdateProjectionMatrix(unsigned int width, unsigned int height); //Updates the projection matrix
+	void MouseInput(float xAxis, float yAxis); //Handles mouse input
 };
