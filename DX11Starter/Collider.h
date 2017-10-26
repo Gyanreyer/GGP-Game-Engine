d35958697977250d6a4070984d7b93ffdc68d9bf
Josh Malmquist <joshmalmquist96@gmail.com>
2017-10-26 15:33:12 -0400

    Added Classes for Player and PRojectile and collision

--- DX11Starter/Collider.h
+++ DX11Starter/Collider.h
@@ -1,9 +1,23 @@
 //Colliders for entities
 #pragma once
 
+enum ColliderType {
+BOX,
+SPHERE,
+MESH, //probably will never implement
+NONE
+};
+
 class Collider
 {
 public:
 	Collider(); //Constructor
 	~Collider(); //Destructor
+	bool CheckCollision(Collider objCollider);
+
+
+private:
+	float width; //Collider Width
+	float height;
+
 };
