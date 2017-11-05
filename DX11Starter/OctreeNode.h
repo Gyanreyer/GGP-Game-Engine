d69b7d5796eb13d4dab76f78cb7b601d0bed6481
Ryan Geyer <gyanreyer@gmail.com>
2017-11-04 23:14:50 -0400

    Added OctreeNode class for spatial partitioning, just need to implement

---
+++ DX11Starter/OctreeNode.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <vector>
+#include "GameObject.h"
+
+class OctreeNode
+{
+public:
+	OctreeNode(XMFLOAT3 centerPoint, float boundingRadius, OctreeNode* parentNode);
+	~OctreeNode();
+
+	GameObject * GetObjects();
+	unsigned int GetObjectCount();
+
+	void AddObject(GameObject * obj);
+
+	bool IsLeafNode();
+
+	void Update();
+
+private:
+	vector<GameObject*> objects;
+
+	OctreeNode * parent;
+	OctreeNode * children;
+
+	XMFLOAT3 center;
+	float radius;
+
+	int GetContainingChildIndex(XMFLOAT3 pos, XMFLOAT3 dimensions);
+	bool MoveObject(GameObject* obj);
+
+	XMFLOAT3 GetMinPt();
+	XMFLOAT3 GetMaxPt();
+
+	int maxItems = 7;
+	float minBoundsSize = 1;
+};
+
